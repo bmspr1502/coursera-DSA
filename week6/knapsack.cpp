fbf6639cdffbff6697f4ac640bcbfbab1f022eba
Pranava Raman <bmspr1502@gmail.com>
2020-12-12 21:18:43 +0530

    problem 1

--- week6/knapsack.cpp
+++ week6/knapsack.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 
-using std::vector;
+using namespace std;
+int optimal[10001][301];
 
 int optimal_weight(int W, const vector<int> &w) {
   //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
+  int n=w.size();
+  
+
+  for (int i=0; i<=W; i++){
+    for(int j=0; j<=n; j++){
+      if(i==0 || j==0)
+        optimal[i][j] = 0;
+      else{
+        optimal[i][j] = optimal[i][j-1];
+        if(i>=w[j-1])
+        optimal[i][j] = max(optimal[i-w[j-1]][j-1] + w[j-1], optimal[i][j-1]);
+      }
     }
   }
-  return current_weight;
+
+/*
+  for(int i=0; i<=W; i++){
+    for(int j=0; j<=n; j++){
+      cout<<optimal[i][j]<<"\t";
+    }
+    cout<<endl;
+  }
+*/
+  return optimal[W][n];
 }
 
 int main() {
